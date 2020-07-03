8876097892d5aee8294ee8949239591415977002
Sam <samiam2013@gmail.com>
2020-07-02 22:27:47 -0700

    5 lines, 80 col.

--- short.php
+++ short.php
@@ -1,7 +1,5 @@
-<?php
-$s = array("javascript","Python","java","csharp","cpp",
+<?php $s = array("javascript","Python","java","csharp","cpp",
   "typescript","bash","C_Programming","ruby");
-foreach($s as $b){ $l[$b] = "http://reddit.com/r/".$b."/top.json?t=week";}
-foreach($l as $b => $k){
-  $r = json_decode(file_get_contents($k))->data->children[0]->data;
+foreach($s as $b){ $l = "http://reddit.com/r/".$b."/top.json?t=week";
+  $r = json_decode(file_get_contents($l))->data->children[0]->data;
   echo 'r/'.$b.": ".html_entity_decode($r->title)."\n".$r->url."\n";}
